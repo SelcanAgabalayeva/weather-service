3dc1cd7aa9939aa737b83f9a1c5dfc86f3d9eebe
SelcanAgabalayeva <selcanagabalayeva12@gmail.com>
2026-06-03 22:10:59 +0400

    fix dotenv crash for railway

--- src/main/java/com/selcan/weather_service/WeatherServiceApplication.java
+++ src/main/java/com/selcan/weather_service/WeatherServiceApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherServiceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
 
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
